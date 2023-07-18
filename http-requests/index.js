e69b3400df40c44384f8db3d9950e70538244ea0
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-18 22:23:20 +0200

    add respose

---
+++ http-requests/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Updated TRATATAT</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
