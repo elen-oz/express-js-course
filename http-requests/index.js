1c010711f99cc3de31b0b384af7cecb60e28866f
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-18 22:24:52 +0200

    rout about

--- http-requests/index.js
+++ http-requests/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('<h1>Updated TRATATAT</h1>');
 });
 
+app.get('/about', (req, res) => {
+  res.send('<h2>About</h2>');
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
