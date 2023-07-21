72781fee7728837ff92b5654d0fa82efe1b8de48
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-21 17:21:22 +0200

    index3

--- 4-middleware/index2.js
+++ 4-middleware/index2.js
@@ -1,10 +1,12 @@
-import express from "express";
+import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 const port = 3000;
+app.use(morgan('tiny'));
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.get('/', (req, res) => {
+  res.send('Hello');
 });
 
 app.listen(port, () => {
